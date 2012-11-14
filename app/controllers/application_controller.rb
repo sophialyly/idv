b1e7586530456d864199ca82a0183c769d614d05
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2012-11-13 20:36:22 -0600

    subiendo cambios en loggin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,13 @@ class ApplicationController < ActionController::Base
   before_filter :authenticate_user!
   helper_method :authenticate!
   
+  
   def authenticate!
     authenticate_or_request_with_http_basic do |username, password|
-      username == "remalogosshekina" && password == "livingadministratore54321ap0985432176543212"
+      username == "remalogosshekina" && password == "livingadministratore54321ap0985432176543212"      
     end
+    
   end
+  
+  
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   skip_before_filter :authenticate_user!, :only => [:index]
+  skip_before_filter :authenticate!, :only => [:index]
   def index
     if current_user
        redirect_to videos_path
