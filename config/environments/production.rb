1df1adcdad849cab3383993225e4522b7c39daca
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2012-11-28 22:06:45 -0600

    changin deprecation rails3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Idv::Application.configure do
   config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners
-  config.active_support.deprecation = :notify
+  config.active_support.deprecation = :silence
    ActionMailer::Base.smtp_settings = {
         :address        => '50.116.21.144',
         :port           => '25',
