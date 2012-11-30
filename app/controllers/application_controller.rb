b9953b4b5eec30d0d0e9f10243bcafc78e7b3be9
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2012-11-30 12:47:56 -0600

    redireccionando la aplicación hacia el dominio correcto

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,33 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :authenticate_user!
+  before_filter :redirect
+  before_filter :authenticate_user!  
   helper_method :authenticate!
-  
-  
+  helper_method :current_url
+  def redirect
+    case 
+      when Rails.env == "production"
+        @url = current_url
+        if @url == "http://www.livingchannel.org"
+        else
+          redirect_to "http://www.livingchannel.org"
+        end
+      when Rails.env == "developement"
+      else
+      end  
+        
+  end
   def authenticate!
     authenticate_or_request_with_http_basic do |username, password|
       username == "livingmx" && password == "livin777"      
     end
     
   end
+  
+  def current_url
+   request.url
+  end
+
   
   
 end
