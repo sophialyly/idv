9f8bbac36c59d395a9e79dd89c97767116801089
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2012-11-30 12:54:34 -0600

    quitando redireccionamiento fallido

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :redirect
   before_filter :authenticate_user!  
   helper_method :authenticate!
   helper_method :current_url
-  def redirect
-    case 
-      when Rails.env == "production"
-        @url = current_url
-        if @url == "http://www.livingchannel.org"
-        else
-          redirect_to "http://www.livingchannel.org"
-        end
-      when Rails.env == "developement"
-      else
-      end  
+
         
   end
   def authenticate!
