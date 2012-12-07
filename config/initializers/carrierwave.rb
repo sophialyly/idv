edaf6f470d0f6620673cec6f5e995aad8bdd8ac6
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2012-12-07 16:57:22 -0600

    cambiando bucket

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,10 +8,9 @@ CarrierWave.configure do |config|
        :aws_access_key_id      => 'AKIAISIGOKLLQ6T5H3NQ',       
        :aws_secret_access_key  => '8gAD87HsktqMUamIQNZnrs5elIsjkAaYwmfoT',
        :region                 => 'us-east-1',
-       :bucket                 => 'idv-nontouch',
        :endpoint               => 'http://idv-nontouch.s3-website-us-east-1.amazonaws.com/'
      }
      
-     config.fog_directory  = 'uploads'                                # required
+     config.fog_directory  = 'idv-nontouch'                                # required
 
 end
