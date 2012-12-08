c80491a3b03c6ab464e2378ab36052a4bb085cf6
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2012-12-07 18:46:02 -0600

    subiendo cambios

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ CarrierWave.configure do |config|
        :provider               => 'AWS',       
        :aws_access_key_id      => 'AKIAISIGOKLLQ6T5H3NQ',       
        :aws_secret_access_key  => '8gAD87HsktqMUamIQNZnrs5elIsjkAaYwmfoT',
-       :region                 => 'eu-east-1'
+       :region                 => 'eu-east-1',
        :endpoint               => 'http://idv-nontouch.s3-website-us-east-1.amazonaws.com/',
        :hostid                 => 'tJIOPcrfMjnwBOJIKDsaHGMNUiUm+URRKtBKHq4Dg9IW2yE1eBYB5hcYJSgkibV5'
      }
