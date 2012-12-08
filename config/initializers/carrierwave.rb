85f8493e0344eaafc772be56f8fa3f9f7029005b
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2012-12-07 20:29:21 -0600

    subiendo al buquetqefasfafedadd

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,15 +3,3 @@
      :secret_access_key => '8gAD87HsktqMUamIQNZnrs5elIsjkAaYwmfoT'
    )
    
-   CarrierWave.configure do |config|
-     config.fog_credentials = {
-       :provider               => 'AWS',       # required
-       :aws_access_key_id      => 'AKIAISIGOKLLQ6T5H3NQ',       # required
-       :aws_secret_access_key  => '8gAD87HsktqMUamIQNZnrs5elIsjkAaYwmfoT',       # required
-       :region                 => 'us-east-1'  # optional, defaults to 'us-east-1'
-     }
-     config.fog_directory  = 'idv-nontouch'
-   end
-   
-
-     
