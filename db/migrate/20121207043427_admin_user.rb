c777b220f487816b0195f7da316e0db76dabeba5
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2012-12-08 00:38:02 -0600

    cambiando migraciones

--- db/migrate/20121207043427_admin_user.rb
+++ db/migrate/20121207043427_admin_user.rb
@@ -1,11 +1,11 @@
 class AdminUser < ActiveRecord::Migration
   def up
     ##### primer administrador de la red ######
-    @first_admin = User.find(1)
-    @first_admin.update_attribute(:role, "admin")
+   # @first_admin = User.find(1)
+   #  @first_admin.update_attribute(:role, "admin")
     #### segundo admin de la red #####
-    @second_admin = User.find(2)
-    @second_admin.update_attribute(:role, "admin")
+   # @second_admin = User.find(2)
+  #  @second_admin.update_attribute(:role, "admin")
   end
 
   def down
