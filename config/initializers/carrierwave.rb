d25422e6c5b9fde3d0aba7f69b6d16e7aba2648c
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2012-12-07 22:42:25 -0600

    subiendo cambio en bucketdadasdderfasdadad

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,13 @@
+   CarrierWave.configure do |config|
+     config.fog_credentials = {
+       :provider               => 'AWS',                                                    # required
+       :aws_access_key_id      => 'AKIAISIGOKLLQ6T5H3NQ',                                  # required
+       :aws_secret_access_key  => '8gAD87HsktqMUamIQNZnrs5elIsjkAaYwmfoT',                # required
+       :region                 => 'us-east-1',                                           # optional, defaults to 'us-east-1'
+       :endpoint               => 'https://igdivi.s3-website-us-east-1.amazonaws.com/ ' # optional, defaults to nil
+     }
+     config.fog_directory  = 'igdivi'                                # required
+     config.fog_public     = false                                   # optional, defaults to true
+     config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+   end
+   
