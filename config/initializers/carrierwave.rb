e7709ccf48121006434676b89f60c97b1e2ab081
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2012-12-07 20:02:18 -0600

    haciendo pruebas4

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,22 +1,10 @@
    AWS::S3::Base.establish_connection!(
      :access_key_id     => 'AKIAISIGOKLLQ6T5H3NQ',
      :secret_access_key => '8gAD87HsktqMUamIQNZnrs5elIsjkAaYwmfoT'
+     :buquet
    )
-  
-   CarrierWave.configure do |config|
-      config.access_key_id     = "AKIAISIGOKLLQ6T5H3NQ" 
-      config.secret_access_key = "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" 
-      #config.s3_bucket            = "cursame-#{Rails.env}"
-      config.bucket            = "idv-nontouch"
-      config.storage(Rails.env.test? || Rails.env.development? ? :file : :s3) 
-   end
-
-   CarrierWave.configure do |config|
-     config.root      = Rails.root.join('tmp')
-     config.cache_dir = 'uploads/cached-carrierwave'
-   end
-
-   Rails.configuration.middleware.delete('Sass::Plugin::Rack')
-   Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Sass::Plugin::Rack')
-   Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static', :urls => ['/uploads'], :root => 'tmp')
    
+   def buquet
+     music_bucket = Bucket.find('idv-nontouch')
+   end
+     
