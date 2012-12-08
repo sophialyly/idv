03070e852e178529c2449d6a760b0d2c2c63c5a6
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2012-12-07 22:24:52 -0600

    subiendo cambio en bucket

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -4,9 +4,9 @@
        :aws_access_key_id      => 'AKIAISIGOKLLQ6T5H3NQ',                        # required
        :aws_secret_access_key  => '8gAD87HsktqMUamIQNZnrs5elIsjkAaYwmfoT',                        # required
        :region                 => 'us-east-1',                   # optional, defaults to 'us-east-1'
-       :endpoint               => 'http://idv-nontouch.s3-website-us-east-1.amazonaws.com/ ' # optional, defaults to nil
+       :endpoint               => 'https://igdivi.s3-website-us-east-1.amazonaws.com/ ' # optional, defaults to nil
      }
-     config.fog_directory  = 'idv-nontouch'                          # required
+     config.fog_directory  = 'igdivi'                          # required
      config.fog_public     = false                                   # optional, defaults to true
      config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
    end
