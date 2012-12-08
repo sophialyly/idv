f4254c4d6eeff209810445b0f300ae7e02c134c3
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2012-12-07 20:17:24 -0600

    subiendo al buquetqefasfafe

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,8 @@
    AWS::S3::Base.establish_connection!(
      :access_key_id     => 'AKIAISIGOKLLQ6T5H3NQ',
      :secret_access_key => '8gAD87HsktqMUamIQNZnrs5elIsjkAaYwmfoT'
+     :bucket => Bucket.find('idv-nontouch')
    )
    
-   def buquet
-     bucket = Bucket.find('idv-nontouch')
-   end
+
      
