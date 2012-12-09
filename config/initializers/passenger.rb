e1bd443a03c7ca9e80f7ec4fab733a3e0f251895
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2012-12-08 23:01:13 -0600

    subiendo slar cambios a passenger

--- config/initializers/passenger.rb
+++ config/initializers/passenger.rb
@@ -1,6 +1,6 @@
-if ENV['production'] && ENV['production'].include?('rvm')
+if ENV['MY_RUBY_HOME'] && ENV['MY_RUBY_HOME'].include?('rvm')
  begin
- rvm_path&nbsp;&nbsp;&nbsp;&nbsp; = File.dirname(File.dirname(ENV['production']))
+ rvm_path&nbsp;&nbsp;&nbsp;&nbsp; = File.dirname(File.dirname(ENV['MY_RUBY_HOME']))
  rvm_lib_path = File.join(rvm_path, 'lib')
  $LOAD_PATH.unshift rvm_lib_path
  require 'rvm'
@@ -11,5 +11,5 @@ if ENV['production'] && ENV['production'].include?('rvm')
  end
 end
 
-ENV['production'] = File.expand_path('../Gemfile', File.dirname(__FILE__))
+ENV['BUNDLE_GEMFILE'] = File.expand_path('../Gemfile', File.dirname(__FILE__))
 require 'bundler/setup'
