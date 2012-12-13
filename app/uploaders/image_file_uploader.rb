2dbcc6b866ad8e0c0ad221ef59449eee70d89665
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2012-12-13 00:06:33 -0600

    intentando redireccionar el lugar de la cache para archivos en carrierwave

--- app/uploaders/image_file_uploader.rb
+++ app/uploaders/image_file_uploader.rb
@@ -30,6 +30,9 @@ class ImageFileUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-
+  
+  def cache_dir
+      "#{Rails.root}/tmp/uploads"
+  end
  
 end
