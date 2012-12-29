2fde32db15e1b9cd34f2207a52eac57545a6136d
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2012-12-28 22:28:09 -0600

    Revert "Revert "Revert "Revert "Revert "non to update"""""
    This reverts commit 2a91d1963d52827c80ea8bf188757c6976fc0ae5.

--- db/schema.rb
+++ db/schema.rb
@@ -14,37 +14,37 @@
 ActiveRecord::Schema.define(:version => 20121207173827) do
 
   create_table "docuent_files", :force => true do |t|
-    t.string    "title"
-    t.text      "description"
-    t.string    "image_file"
-    t.string    "documents"
-    t.timestamp "created_at",  :null => false
-    t.timestamp "updated_at",  :null => false
+    t.string   "title"
+    t.text     "description"
+    t.string   "image_file"
+    t.string   "documents"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   create_table "photos", :force => true do |t|
-    t.string    "title"
-    t.string    "image_file"
-    t.string    "add_gallery"
-    t.timestamp "created_at",  :null => false
-    t.timestamp "updated_at",  :null => false
+    t.string   "title"
+    t.string   "image_file"
+    t.string   "add_gallery"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   create_table "users", :force => true do |t|
-    t.string    "email",                  :default => "", :null => false
-    t.string    "encrypted_password",     :default => "", :null => false
-    t.string    "reset_password_token"
-    t.timestamp "reset_password_sent_at"
-    t.timestamp "remember_created_at"
-    t.integer   "sign_in_count",          :default => 0
-    t.timestamp "current_sign_in_at"
-    t.timestamp "last_sign_in_at"
-    t.string    "current_sign_in_ip"
-    t.string    "last_sign_in_ip"
-    t.timestamp "created_at",                             :null => false
-    t.timestamp "updated_at",                             :null => false
-    t.string    "role"
-    t.string    "status"
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+    t.string   "role"
+    t.string   "status"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
