94eff17925d82ef981211157248c2d11e693ff93
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2013-07-30 16:29:43 -0500

    cambios en mailers

--- app/controllers/masive_mailer_controller.rb
+++ app/controllers/masive_mailer_controller.rb
@@ -1,11 +1,11 @@
 class MasiveMailerController < ApplicationController
   def masive_send_mailer
-     @message = params[:message]
-     @users = User.all
-     @users.each do |user|
-         @user = user
-         @mail_n = UserNotify.masive_mailer(@user, @message).deliver
+     message = params[:message]
+     user = User.find_by_email('jose_alfredo@cursa.me')
+   #  @users = User.all
+    # @users.each do |user|
+         @mail_n = UserNotify.masive_mailer(user, message).deliver
          puts @mail_n
-     end
+    # end
   end
 end
